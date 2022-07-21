f1b57f523829b3b9990b4ccc622befea22114b41
Abhishek tomar <abhishektomarcpr@gmail.com>
2022-07-22 01:33:10 +0530

    day3_product_data

--- producData/products.js
+++ producData/products.js
@@ -178,6 +178,183 @@
                   retaing: "&star &star &star &star &star",
                   brand:"Bowers&Wilkins", 
                   category:"earphone"},
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/Marshall-Major-4-Black-2_400x.jpg?v=1616481617",
+                    title: "MARSHALL-MAJOR IV",
+                    info: "Wireless Headphone",
+                    price: 1299,
+                    retaing: "&star &star &star &star &star",
+                    brand:"1Custom" ,
+                    category:"headphone"
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/Marsahll-B_B-02_400x.jpg?v=1617627383",
+                    title: "MARHALL- EMBORTON",
+                    info: "Portable Bluetooth Speakers",
+                    price: 14499,
+                    retaing: "&star &star &star &star &star",
+                    brand:"Bowers&Wilkins" ,
+                    category:"headphone"
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/Marshall-Stockwell-2-Black-1_400x.jpg?v=1584357293",
+                    title: "MARSHALL-STOCKWILL II",
+                    info: "PORTABLE BLUETOOTH SPEAKER",
+                    price: 21999,
+                    retaing: "&star &star &star &star &star",
+                    brand:"1Custom" ,
+                    category:"headphone"
+                    
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/marshall-stanmore-ii-headphone-zone-11644501917759_400x.jpg?v=1613721864",
+                    title: "MARSHALL- STANMORE II",
+                    info: "Wireless Home Speakers",
+                    price: 34999,
+                    retaing: "&star &star &star &star &star",
+                    brand:"Bowers&Wilkins" ,
+                    category:"headphone"
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/marshall-acton-ii-bt-headphone-zone-5033976299583_400x.jpg?v=1637661920",
+                    title: "MARSHALL- ACTION II BT",
+                    info: "Wireless Home Speakers",
+                    price: 26999,
+                    retaing: "&star &star &star &star &star",
+                    brand:"1Custom" ,
+                    category:"headphone"
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/Black_Brass-02_400x.jpg?v=1628685261",
+                    title: "MARSHALL- KILBURN II",
+                    info: "Wireless Home Speakers",
+                    price: 29999,
+                    retaing: "&star &star &star &star &star",
+                    brand:"Bowers&Wilkins" ,
+                    category:"headphone"
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/marshall-woburn-ii-headphone-zone-5032063860799_400x.jpg?v=1613727432",
+                    title: "MARSHALL WOUBURN II",
+                    info: "Wireless Home Speakers",
+                    price: 49999,
+                    retaing: "&star &star &star &star &star",
+                    brand:"1Custom" ,
+                    category:"headphone"
+                  
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Marshall-Monitor-II-ANC-1160-1160-06_400x.jpg?v=1630743116",
+                    title: "MARSHALL - MONITOR II A.N.C.",
+                    info: "Wireless Headphone",
+                    price: 2499,
+                    retaing: "&star &star &star &star &star",
+                    brand:"Bowers&Wilkins" ,
+                    category:"headphone"
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/marshall-mode-eq-headphone-zone-2590658265151_400x.jpg?v=1626357046",
+                    title: "MARSHALL - MODE EQ",
+                    info: "In-Ears With 1 Dynamic Driver",
+                    price: 5999,
+                    retaing: "&star &star &star &star &star",
+                    brand:"1Custom" ,
+                    category:"headphone"
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Marshall-Tufton-Black_Brass-02_400x.jpg?v=1654676063",
+                    title: "MARSHALL - TUFTON",
+                    info: "Portable Bluetooth Speakers",
+                    price: 44999,
+                    retaing: "&star &star &star &star &star",
+                    brand:"Bowers&Wilkins" ,
+                    category:"headphone"
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Marshall-Minor-III-1_400x.jpg?v=1654591947",
+                    title: "MARSHALL - MINOR III",
+                    info: "True Wireless Earbuds",
+                    price: 11999,
+                    retaing: "&star &star &star &star &star",
+                    brand:"1Custom" ,
+                    category:"headphone"
+                    
+                  },
+                  {
+                    image:
+                      "https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Marshall-Uxbridge-Voice-11_400x.jpg?v=1654941760",
+                    title: "MARSHALL - UXBRIDGE VOICE WITH AMAZON ALEXA",
+                    info: "bluetooth speaker",
+                    price: 21999,
+                    retaing: "&star &star &star &star &star",
+                    brand:"Bowers&Wilkins" ,
+                    category:"headphones"
+                  },
+                    {
+                      image:
+                        "https://cdn.shopify.com/s/files/1/0153/8863/products/KZ-ZSN-Pro-X-Black-02_596bf1c5-9f86-4e25-a29b-bc5422a38160_400x.jpg?v=1650864564",
+                      title: "MARSHALL - UXBRIDGE VOICE WITH AMAZON ALEXA",
+                      info: "In-Earphones",
+                      price: 4999,
+                      retaing: "&star &star &star &star &star",
+                      brand:"Bowers&Wilkins" ,
+                    category:"earphone"},
+                      {
+                        image:
+                          "https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Final-Audio-E1000-BALCK-No-Mic-03_400x.jpg?v=1632819650",
+                        title: "MARSHALL - UXBRIDGE VOICE ALMANDIO ALEXA",
+                        info: "bluetooth speaker",
+                        price: 27999,
+                        retaing: "&star &star &star &star &star",
+                        brand:"Bowers&Wilkins", 
+                        category:"earphone"},
+                        {
+                          image:
+                            "https://cdn.shopify.com/s/files/1/0153/8863/products/4be3c481986e40ba9c3133b879bdae48_400x.jpg?v=1650872939",
+                          title: "MARSHALL - UXBRIDGE VOICE ALMANDIO ALEXA",
+                          info: "bluetooth speaker",
+                          price: 27999,
+                          retaing: "&star &star &star &star &star",
+                          brand:"1custom", 
+                          category:"earphone"},
+                          {
+                            image:
+                              "https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Final-Audio-E1000-BALCK-No-Mic-03_400x.jpg?v=1632819650",
+                            title: "MARSHALL - UXBRIDGE VOICE ALMANDIO ALEXA",
+                            info: "bluetooth speaker",
+                            price: 27999,
+                            retaing: "&star &star &star &star &star",
+                            brand:"Bowers&Wilkins", 
+                            category:"earphone"},
+                            {
+                              image:
+                                "https://cdn.shopify.com/s/files/1/0153/8863/products/4be3c481986e40ba9c3133b879bdae48_400x.jpg?v=1650872939",
+                              title: "MARSHALL - UXBRIDGE VOICE ALMANDIO ALEXA",
+                              info: "bluetooth speaker",
+                              price: 27999,
+                              retaing: "&star &star &star &star &star",
+                              brand:"1custom", 
+                              category:"earphone"},
+                              {
+                                image:
+                                  "https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Final-Audio-E1000-BALCK-No-Mic-03_400x.jpg?v=1632819650",
+                                title: "MARSHALL - UXBRIDGE VOICE ALMANDIO ALEXA",
+                                info: "bluetooth speaker",
+                                price: 27999,
+                                retaing: "&star &star &star &star &star",
+                                brand:"Bowers&Wilkins", 
+                                category:"earphone"},
             
   ];
 
@@ -234,8 +411,29 @@ window.addEventListener("load",load)
       displayTrandingNow(arr);
     }
 
+document.getElementById("sort").addEventListener("change",function(){
+  let val=document.getElementById("sort").value;
+  console.log(val);
+  if(val=="Lth"){
+     arr=arr.sort(function(a,b){
+      console.log(a.price,b.price)
+      return +a.price- (+b.price)
+    })
+  displayTrandingNow(arr)
+  }
+  else{
+    arr=arr.sort(function(a,b){
+      console.log(a.price,b.price)
+      return +b.price- (+a.price)
+    })
+    displayTrandingNow(arr)
+  }
+})
+
+
  
     function displayTrandingNow(arr) {
+      console.log(arr)
      
       document.querySelector("#container3").innerText=""
       arr.forEach(function (elm,index) {
